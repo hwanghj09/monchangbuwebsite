b23aa6243116f1d1bea7482d6cdda290551da075
hwanghj09 <hwanghj09@naver.com>
2025-03-22 22:41:18 +0900

    /index

--- server.js
+++ server.js
@@ -100,6 +100,9 @@ function isLoggedIn(req, res, next) {
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+app.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
 
 app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'login.html'));
